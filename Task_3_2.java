077e3593e4c129d895c1cf3b8395f71679bffdd2
Savchits Tatiana <savchits@farzoom.com>
2023-09-20 23:59:09 +0300

    3.2 Решена

--- Task_3_2.java
+++ Task_3_2.java
@@ -1,12 +1,9 @@
 package YandexHandBook;
-import java.util.Iterator;
-import java.util.Scanner;
-import java.util.LinkedList;
-import java.util.Comparator;
+import java.util.*;
 
 class CompInter implements  Comparator<Interval>{
     public int compare(Interval p1, Interval p2){
-        return Integer.compare(p1.getStart(), p2.getStart());
+        return Integer.compare(p1.getFinish(), p2.getFinish());
     }
 }
 class Interval{
@@ -23,7 +20,22 @@ class Interval{
         this.v1=v1;
         this.v2=v2;
     }
+
+    @Override
+    public int hashCode() {
+            return (this.v1 * this.v2)/(this.v2+this.v1);//деление на 0
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Interval) {
+            Interval inter = (Interval) obj;
+            boolean res = (this.v1 == inter.getStart() && this.v2 == inter.getFinish());
+            return res;
+        } else return false;
+    }
 }
+
 public class Task_3_2 {
         public static void main(String[] args) {
             Scanner scan = new Scanner(System.in);
@@ -34,22 +46,27 @@ public class Task_3_2 {
                 int b = scan.nextInt();
                 list.add(new Interval(a, b));
             }
+
+            //удалить повторяющиеся слоты
+            Set<Interval> set = new HashSet<>(list);
+            list=new LinkedList<Interval>(set);
+
             CompInter comp = new CompInter();
-            list.sort(comp.reversed());
+            list.sort(comp);
             LinkedList<Interval> resultList = new LinkedList<>();
             while (!list.isEmpty()) {
-                Interval resultPart;
-                int finish = 100;
-                for(Interval inter : list){
-                    int v2 = inter.getFinish();
-                    if(v2<finish) {
-                        resultPart = inter;
-                        finish=v2;
+                Interval current = list.getFirst();
+                resultList.add(current);
+                int check=current.getFinish();
+                list.remove(current);
+                Iterator it =list.iterator();
+                while (it.hasNext()){
+                    Interval inter = (Interval) it.next();
+                    if(inter.getStart()<=check){
+                         it.remove();//list.remove(inter);
                     }
                 }
-               /* if(resultPart.!=null)
-                    resultList.add(resultPart);*/
-
             }
+            System.out.println(resultList.size());
         }
     }
